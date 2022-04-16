271a12980fda6d342fcb900e9ad0d9b5a09e0b81
Igor <igorkaluga00@gmail.com>
2022-04-15 21:31:23 -0700

    createBookCard func added - removed from displayLibrary loop

--- script.js
+++ script.js
@@ -12,39 +12,46 @@ function addBookToLibrary(book, array) {
 };
 
 // DOM
-
 const libraryContainer = document.getElementById('library-container');
 
+function createBookCard(book) {
+    const bookContainer = document.createElement('div');
+    const bookTitle = document.createElement('h2');
+    const bookAuthor = document.createElement('p');
+    const pageCount = document.createElement('p');
+    const readStatus = document.createElement('p');
 
-// Book Container elements
+    bookTitle.innerHTML = book.title;
+    bookAuthor.innerHTML = book.author;
+    pageCount.innerHTML = book.pageCount;
+    readStatus.innerHTML = book.readStatus;
 
+    bookContainer.appendChild(bookTitle);
+    bookContainer.appendChild(bookAuthor);
+    bookContainer.appendChild(pageCount);
+    bookContainer.appendChild(readStatus);
+    libraryContainer.appendChild(bookContainer);
+}
 
 function displayLibray() {
-    for (let book in myLibrary) {
-        const bookContainer = document.createElement('div');
-        const bookTitle = document.createElement('h2');
-        const bookAuthor = document.createElement('p');
-        const pageCount = document.createElement('p');
-        const readStatus = document.createElement('p');
-
-        bookTitle.innerHTML = myLibrary[book].title;
-        bookAuthor.innerHTML = myLibrary[book].author;
-        pageCount.innerHTML = myLibrary[book].pageCount;
-        readStatus.innerHTML = myLibrary[book].readStatus;
-
-        bookContainer.appendChild(bookTitle);
-        bookContainer.appendChild(bookAuthor);
-        bookContainer.appendChild(pageCount);
-        bookContainer.appendChild(readStatus);
-        libraryContainer.appendChild(bookContainer);
+    for (let book of myLibrary) {
+        createBookCard(book);
     }
 }
 /*
 We have an array that stores book objects.
 There is a function that pushes book objects into the array.
 
-function that loops through the array and displays array objects onto the html doc.
-create div element -> add array data to div -> append div to index.html
+Run on load to display books already in the array. use local storage:
+    function that loops through the array and displays array objects onto the html doc.
+    create div element -> add array data to div -> append div to index.html
+
+add a button that pull up a form.
+form creates book object.
+adds book to the array ->
+displays the newly added book through createBookCard()
+
+
 
 */
 
